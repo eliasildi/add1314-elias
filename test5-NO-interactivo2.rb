486ccdeb7e6e995b220559e8f1561aba4c247824
profesor <profesor@ies.ies>
2013-09-24 09:52:44 +0100

    subir todo

--- test5-NO-interactivo2.rb
+++ test5-NO-interactivo2.rb
@@ -2,12 +2,9 @@
 # encoding: UTF-8
 
 action=ARGV.first
-puts a
-# ponemos action=1 para asignarle
+puts action
 
 if action=="crear" then
-# este action==1 lleva dos == porque queremos que compare el valor de la variable,
-# no es que lo estemos asignando de nuevo
 	system("mkdir curso2")
 	system("mkdir curso3")
 	system("mkdir curso4")
@@ -16,9 +13,10 @@ if action=="crear" then
 	system("mkdir curso1/ssoo -p")
 	system("mkdir curso1/fol -p")
 	system("chmod 700 curso*/*")
-end
-
-if action=="eliminar" then
+	
+elsif action=="eliminar" then
 	system("rm -r curso2")
 	puts "carpetas eliminadas"
+else
+	puts "no entiendo <"+action+">"
 end
