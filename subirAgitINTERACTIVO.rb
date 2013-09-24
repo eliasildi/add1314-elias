45726e7020ecc91ca148178ba1ae9aae9cf7850d
profesor <profesor@ies.ies>
2013-09-24 10:31:09 +0100

    up all data files

--- subirAgitINTERACTIVO.rb
+++ subirAgitINTERACTIVO.rb
@@ -2,14 +2,14 @@
 # encoding: UTF-8
 
 
-print "¿Qué quieres?"
+puts "¿Qué quieres? "
 action=gets.chomp()
 
 if action=="subir" then
 	system("git add *")
 	system("git commit -m 'up all data files'")
 	system("git push")
-	puts "subido"
+	puts "subidoooooooooooooooooooooooooooo con éxitoooo"
 	
 elsif action=="bajar" then
 	system("git pull")
