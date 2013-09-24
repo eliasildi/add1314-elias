bdfdb261a13d83e09bcf2f96851c838110e2f3f0
profesor <profesor@ies.ies>
2013-09-24 10:41:45 +0100

    AUTO up all data files

--- subirAgit.rb
+++ subirAgit.rb
@@ -6,7 +6,7 @@ puts action
 
 if action=="subir" then
 	system("git add *")
-	system("git commit -m 'up all data files'")
+	system("git commit -m 'AUTO up all data files'")
 	system("git push")
 	puts "subido"
 	
--- subirAgitINTERACTIVO.rb
+++ subirAgitINTERACTIVO.rb
@@ -7,7 +7,7 @@ action=gets.chomp()
 
 if action=="subir" then
 	system("git add *")
-	system("git commit -m 'up all data files'")
+	system("git commit -m 'AUTO up all data files'")
 	system("git push")
 	puts "*********Subidoooooooooooooooooooooooooooo con éxitoooo****"
 	
