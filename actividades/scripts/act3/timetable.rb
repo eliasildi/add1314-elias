70b0908e606d9175afa57b72bcfc2de6b9809dd8
eliasildi <eliasdetenerife@gmail.com>
2013-11-12 10:01:44 +0000

    update

--- actividades/scripts/act3/timetable.rb
+++ actividades/scripts/act3/timetable.rb
@@ -2,7 +2,21 @@
 #
 # este ira en /usr/local/bin y sera llamado desde .bashhrc
 #
-puts "Ejecutando #{$0}..."
+#iniciamos el programa
+TIMECOMMAND='date +%H%M' #creamos una variable para almacenar la fecha
+EXITCOMMAND='exit' #EXITCOMMAND SIRVE PARA windows por lo que lo usaremos
 
-sTime='date +%H%M'
-puts sTime
+
+
+
+
+puts "Ejecutando #{$0}..." #nos muestra un mernsaje al iniciarse
+
+sTime=TIMECOMMAND
+puts "HoraMinuto=#{sTime}" #Mostramos la fecha hora
+
+if (sTime < "1040" and sTime < "1115" ) then
+		puts "Vete asl recreo!!!"
+		system (EXITCOMMAND)
+end
+
