10ec63662074d772fb1878dc10812b103d7b01e6
eliasildi <eliasdetenerife@gmail.com>
2014-01-24 09:00:55 +0000

    update

---
+++ scripRUBY/subirAgitINTERACTIVO.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+#hjklñ
+
+puts "¿Qué quieres? "
+action=gets.chomp()
+
+if action=="subir" then
+	system("git add *")
+	system("git commit -m 'AUTO up all data files'")
+	system("git push")
+	puts "*********Subidoooooooooooooooooooooooooooo con éxitoooo****"
+	
+elsif action=="bajar" then
+	system("git pull")
+	puts "descargado con éxito"
+else
+	puts "<"+action+"> no es, prueba otra vez, hay miles de premios."
+end
